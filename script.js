ccc73073696e173d5a5df11bc035b22de2387844
sanukath <124057233+sanukath@users.noreply.github.com>
2023-01-31 18:46:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+ function displayTime(){
+ var dateTime = new Date ();
+ var hrs = dateTime.getHours();
+ var min = dateTime.getMinutes();
+ var sec = dateTime.getSeconds();
+ var session =  document.getElementById('sessions');
+
+document.getElementById('hours').innerHTML =hrs;
+document.getElementById('minutes').innerHTML = min;
+document.getElementById('seconds').innerHTML = sec;
+}
+
+setInterval(displayTime, 10);
+
